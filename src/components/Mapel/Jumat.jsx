b71834bf1e0ef138dc0a18601224e7465baf3617
Anouskaaa <sandydcn29@gmail.com>
2024-09-07 08:10:47 +0700

    Jadwal Pelajaran

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -5,36 +5,43 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium"></div>
-				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
+				<div className="w-[50%] text-base  font-medium">Jumrel</div>
+				<div className="flex justify-center items-center text-sm">06.45-07.30</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium"></div>
-				<div className="flex justify-center items-center text-sm">09.40-10.00</div>
+				<div className="w-[50%] text-base  font-medium">Bahasa Jepang</div>
+				<div className="flex justify-center items-center text-sm">07.30-08.20</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium"></div>
-				<div className="flex justify-center items-center text-sm">10.00-11.20</div>
+				<div className="w-[50%] text-base  font-medium">Fisika</div>
+				<div className="flex justify-center items-center text-sm">08.20-09.10</div>
 			</div>
 			<div
 				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium"></div>
-				<div className="flex justify-center items-center text-sm">11.20-13.00</div>
+				<div className="w-[50%] text-base  font-medium">Istirahat</div>
+				<div className="flex justify-center items-center text-sm">09.10-09.55</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium"></div>
-				<div className="flex justify-center items-center text-sm">13.00-14.20</div>
+				<div className="w-[50%] text-base  font-medium">Matematika Wajib</div>
+				<div className="flex justify-center items-center text-sm">09.55-10.45</div>
+			</div>
+			<div
+				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Biologi</div>
+				<div className="flex justify-center items-center text-sm">10.45-11.35</div>
 			</div>
 		</>
 	)
